6f93b1038b0e35d7724ee2d2c7ab8fce101e0107
Ainomo <167073179+ainomoai@users.noreply.github.com>
2024-07-04 01:18:59 -0700

    Update CIP-3-4.ts

--- scripts/CIP-3-4.ts
+++ scripts/CIP-3-4.ts
@@ -1,4 +1,4 @@
-44444import hre, { deployments, network, hardhatArguments } from "hardhat";
+444444import hre, { deployments, network, hardhatArguments } from "hardhat";
 import { castVote, createProposal, executeProposal, fundMultisign, queueProposal } from "./utils";
 
 async function main() {
